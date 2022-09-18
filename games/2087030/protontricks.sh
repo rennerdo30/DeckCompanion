8caa3f1bfedee8249fec8dc6985fdfd4f4b678a2
rennerdo30 <rennerdo30@gmail.com>
2022-09-19 00:23:12 +0200

    added fixes for Shatterline

---
+++ games/2087030/protontricks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+flatpak install com.github.Matoking.protontricks
+flatpak run com.github.Matoking.protontricks $SteamAppId d3dcompiler_47 win7
+
+protontricks 2087030 d3dcompiler_47 win7
